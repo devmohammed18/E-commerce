542caa5cfbf278d33a17556c248369dbc9a3f852
devmohammed18 <devmohammed18@gmail.com>
2024-12-30 17:58:19 -0700

    create page notFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+
+//import { redirect } from 'next/navigation'
+import Link from 'next/link';
+function NotFound() {
+   // redirect("/");
+
+    return (
+
+        <div className='w-full min-h-screen flex-col  justify-center items-center py-1 space-y-1 mt-[85px] mb-1 bg-[var(--secondry-color)]'>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link className=' py-2 px-4 bg-red-700 ' href="/">Go Home</Link>
+        </div>
+    )
+}
+
+export default NotFound
